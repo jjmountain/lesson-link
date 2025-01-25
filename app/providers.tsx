868fcba93236845a690fc2442f01cb3254ea5f39
James Devereux <devereuxjj@gmail.com>
2025-01-25 12:06:16 +0000

    setup middleware for clerk

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { ModalProvider } from "@/components/modal/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <ClerkProvider>
       <Toaster className="dark:hidden" />
       <Toaster theme="dark" className="hidden dark:block" />
       <ModalProvider>{children}</ModalProvider>
-    </SessionProvider>
+    </ClerkProvider>
   );
 }
