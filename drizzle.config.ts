3c070ac83b827aefd6f3bf00aba3280ca3546153
James Devereux <devereuxjj@gmail.com>
2025-01-26 12:20:33 +0000

    add config to drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv';
 
+if (process.env.NODE_ENV !== 'production') {
+  config({ path: '.env.local' });
+}
 export default defineConfig({
   schema: "./lib/schema.ts",
   out: "./drizzle/migrations",
@@ -7,4 +11,5 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
+
 });
